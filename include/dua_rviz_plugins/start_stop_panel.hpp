a55a77a58aa10135efaf11a739dc77a9ca7aefb5
Alessandro Tenaglia <alessandro.tenaglia42@gmail.com>
2025-02-25 16:44:23 +0100

    feat: improve style

--- include/dua_rviz_plugins/start_stop_panel.hpp
+++ include/dua_rviz_plugins/start_stop_panel.hpp
@@ -1,8 +1,7 @@
 /**
- * Reference of RViz2 StartStopPanel for DUA modules.
+ * StartStopPanel header file.
  *
- * Alessandro Tenaglia <a.tenaglia@dotxautomation.com>
- * Alexandru Cretu <a.cretu@dotxautomation.com>
+ * dotX Automation <info@dotxautomation.com>
  *
  *  February 17, 2025
  */
@@ -23,15 +22,14 @@
  * limitations under the License.
  */
 
-#ifndef DUA_RVIZ_PLUGINS__START_STOP_PANEL_HPP_
-#define DUA_RVIZ_PLUGINS__START_STOP_PANEL_HPP_
+#pragma once
 
+// DUA libraries
 #include <dua_qos_cpp/dua_qos.hpp>
-
+// ROS2 libraries
 #include <rclcpp/rclcpp.hpp>
 #include <rviz_common/panel.hpp>
-#include <std_msgs/msg/empty.hpp>
-
+// Qt libraries
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
@@ -39,8 +37,15 @@
 #include <QLabel>
 #include <QString>
 
+// Messages
+#include <std_msgs/msg/empty.hpp>
+
+#define REPUBLISH_COUNT 10
+
 namespace dua_rviz_plugins
 {
+
+using std_msgs::msg::Empty;
 
 /**
  * @brief Custom button for sending start and stop text messages in RViz.
@@ -51,40 +56,49 @@ class StartStopPanel : public rviz_common::Panel
 
 public:
   /**
-   * @brief Constructor for StartStopPanel.
+   * @brief Constructor
    */
   StartStopPanel(QWidget * parent = nullptr);
 
 private Q_SLOTS:
   /**
-   * @brief Publishes an empty message on the first topic.
+   * @brief Initializes the publisher for the start button.
    */
-  void startButtonClicked();
+  void init_start_pub(const std::string & start_topic_);
+
   /**
-   * @brief Publishes an empty message on the second topic.
+   * @brief Initializes the publisher for the stop button.
    */
-  void stopButtonClicked();
+  void init_stop_pub(const std::string & stop_topic_);
+
+  /**
+   * @brief Publishes an empty message to the start topic.
+   */
+  void start_button_clicked();
+
+  /**
+   * @brief Publishes an empty message to the stop topic.
+   */
+  void stop_button_clicked();
+
   /**
    * @brief Updates the publisher for the start topic when the topic name is changed from the GUI.
    */
-  void updateStartTopic();
+  void update_start_topic();
+
   /**
    * @brief Updates the publisher for the stop topic when the topic name is changed from the GUI.
    */
-  void updateStopTopic();
+  void update_stop_topic();
 
 private:
-  rclcpp::Node::SharedPtr node_; /**< The ROS node. >*/
-  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_start_; /**< The publisher for the first topic. >*/
-  rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr publisher_stop_; /**< The publisher for the second topic. >*/
-  QPushButton * button_start_; /**< Button for starting the process. >*/
-  QPushButton * button_stop_; /**< Button for stopping the process. >*/
-  QLineEdit * topic_start_input_; /**< Input field for the start topic name. >*/
-  QLineEdit * topic_stop_input_; /**< Input field for the stop topic name. >*/
-  QString topic_start_name_; /**< Current start topic name. >*/
-  QString topic_stop_name_; /**< Current stop topic name. >*/
+  rclcpp::Node::SharedPtr node_;
+  QString start_topic_;
+  QString stop_topic_;
+  rclcpp::Publisher<Empty>::SharedPtr start_pub_;
+  rclcpp::Publisher<Empty>::SharedPtr stop_pub_;
+  QLineEdit * start_topic_input_;
+  QLineEdit * stop_topic_input_;
 };
 
 } // namespace dua_rviz_plugins
-
-#endif  // DUA_RVIZ_PLUGINS__START_STOP_HPP_
--- src/start_stop_panel.cpp
+++ src/start_stop_panel.cpp
@@ -1,8 +1,7 @@
 /**
- * Implementation of RViz2 StartStopPanel for DUA modules.
+ * StartStopPanel class source file.
  *
- * Alessandro Tenaglia <a.tenaglia@dotxautomation.com>
- * Alexandru Cretu <a.cretu@dotxautomation.com>
+ * dotX Automation <info@dotxautomation.com>
  *
  *  February 17, 2025
  */
@@ -30,95 +29,117 @@ namespace dua_rviz_plugins
 
 StartStopPanel::StartStopPanel(QWidget * parent)
 : rviz_common::Panel(parent),
-  topic_start_name_("/start"),
-  topic_stop_name_("/stop")
+  start_topic_("/start"),
+  stop_topic_("/stop")
 {
   // Initialize ROS2 node
   node_ = std::make_shared<rclcpp::Node>("rviz_button_node");
 
-  // Create publishers for two different topics
-  publisher_start_ = node_->create_publisher<std_msgs::msg::Empty>(
-    topic_start_name_.toStdString(),
-    dua_qos::Reliable::get_datum_qos());
-  publisher_stop_ = node_->create_publisher<std_msgs::msg::Empty>(
-    topic_stop_name_.toStdString(),
-    dua_qos::Reliable::get_datum_qos());
+  // Create start and stop publishers
+  init_start_pub(start_topic_.toStdString());
+  init_stop_pub(stop_topic_.toStdString());
 
   // Create the UI elements for start topic
-  QLabel* label_start = new QLabel("Start Topic:", this);
-  topic_start_input_ = new QLineEdit(topic_start_name_, this);
-  connect(topic_start_input_, &QLineEdit::editingFinished, this, &StartStopPanel::updateStartTopic);
-  button_start_ = new QPushButton("START", this);
-  connect(button_start_, &QPushButton::clicked, this, &StartStopPanel::startButtonClicked);
-  button_start_->setStyleSheet("color: green; font-weight: bold;");
+  QLabel * start_label = new QLabel("Start Topic:", this);
+  start_topic_input_ = new QLineEdit(start_topic_, this);
+  connect(
+    start_topic_input_,
+    &QLineEdit::editingFinished,
+    this,
+    &StartStopPanel::update_start_topic);
+  QPushButton * start_button = new QPushButton("START", this);
+  start_button->setStyleSheet("color: green; font-weight: bold;");
+  connect(
+    start_button,
+    &QPushButton::clicked,
+    this,
+    &StartStopPanel::start_button_clicked);
 
   // Create the UI elements for stop topic
-  QLabel* label_stop = new QLabel("Stop Topic:", this);
-  topic_stop_input_ = new QLineEdit(topic_stop_name_, this);
-  connect(topic_stop_input_, &QLineEdit::editingFinished, this, &StartStopPanel::updateStopTopic);
-  button_stop_ = new QPushButton("STOP", this);
-  connect(button_stop_, &QPushButton::clicked, this, &StartStopPanel::stopButtonClicked);
-  button_stop_->setStyleSheet("color: red; font-weight: bold;");
+  QLabel * stop_label = new QLabel("Stop Topic:", this);
+  stop_topic_input_ = new QLineEdit(stop_topic_, this);
+  connect(
+    stop_topic_input_,
+    &QLineEdit::editingFinished,
+    this,
+    &StartStopPanel::update_stop_topic);
+  QPushButton * stop_button = new QPushButton("STOP", this);
+  stop_button->setStyleSheet("color: red; font-weight: bold;");
+  connect(
+    stop_button,
+    &QPushButton::clicked,
+    this,
+    &StartStopPanel::stop_button_clicked);
 
-  // Arrange the widgets in layouts
-  auto mainLayout = new QVBoxLayout();
-
-  auto startLayout = new QHBoxLayout();
-  startLayout->addWidget(label_start);
-  startLayout->addWidget(topic_start_input_);
-  startLayout->addWidget(button_start_);
-
-  auto stopLayout = new QHBoxLayout();
-  stopLayout->addWidget(label_stop);
-  stopLayout->addWidget(topic_stop_input_);
-  stopLayout->addWidget(button_stop_);
-
-  mainLayout->addLayout(startLayout);
-  mainLayout->addLayout(stopLayout);
-  setLayout(mainLayout);
+  // Create the main layout
+  auto main_layout = new QVBoxLayout();
+  // Add start layout
+  auto start_layout = new QHBoxLayout();
+  start_layout->addWidget(start_label);
+  start_layout->addWidget(start_topic_input_);
+  start_layout->addWidget(start_button);
+  main_layout->addLayout(start_layout);
+  // Add stop layout
+  auto stop_layout = new QHBoxLayout();
+  stop_layout->addWidget(stop_label);
+  stop_layout->addWidget(stop_topic_input_);
+  stop_layout->addWidget(stop_button);
+  main_layout->addLayout(stop_layout);
+  // Set the main layout
+  setLayout(main_layout);
 }
 
-void StartStopPanel::startButtonClicked()
+void StartStopPanel::init_start_pub(const std::string & start_topic_)
 {
-  // Publish empty message on the first topic
-  std_msgs::msg::Empty empty_msg;
-  for (int i = 0; i < 10; i++) {
-    publisher_start_->publish(empty_msg);
+  start_pub_ = node_->create_publisher<Empty>(
+    start_topic_,
+    dua_qos::Reliable::get_datum_qos());
+}
+
+void StartStopPanel::init_stop_pub(const std::string & stop_topic_)
+{
+  stop_pub_ = node_->create_publisher<Empty>(
+    stop_topic_,
+    dua_qos::Reliable::get_datum_qos());
+}
+
+void StartStopPanel::start_button_clicked()
+{
+  Empty empty_msg;
+  for (int i = 0; i < REPUBLISH_COUNT; i++) {
+    start_pub_->publish(empty_msg);
   }
 }
 
-void StartStopPanel::stopButtonClicked()
+void StartStopPanel::stop_button_clicked()
 {
-  // Publish empty message on the second topic
-  std_msgs::msg::Empty empty_msg;
-  for (int i = 0; i < 10; i++) {
-    publisher_stop_->publish(empty_msg);
+  Empty empty_msg;
+  for (int i = 0; i < REPUBLISH_COUNT; i++) {
+    stop_pub_->publish(empty_msg);
   }
 }
 
-void StartStopPanel::updateStartTopic()
+void StartStopPanel::update_start_topic()
 {
+  // Parse the topic name from the input field
+  QString new_topic_ = start_topic_input_->text().trimmed();
+
   // Update the topic name if it has changed
-  QString new_topic = topic_start_input_->text().trimmed();
-  if (!new_topic.isEmpty() && new_topic != topic_start_name_) {
-    topic_start_name_ = new_topic;
-    publisher_start_.reset();
-    publisher_start_ = node_->create_publisher<std_msgs::msg::Empty>(
-      topic_start_name_.toStdString(),
-      dua_qos::Reliable::get_datum_qos());
+  if (!new_topic_.isEmpty() && new_topic_ != start_topic_) {
+    start_topic_ = new_topic_;
+    init_start_pub(start_topic_.toStdString());
   }
 }
 
-void StartStopPanel::updateStopTopic()
+void StartStopPanel::update_stop_topic()
 {
+  // Parse the topic name from the input field
+  QString new_topic_ = stop_topic_input_->text().trimmed();
+
   // Update the topic name if it has changed
-  QString new_topic = topic_stop_input_->text().trimmed();
-  if (!new_topic.isEmpty() && new_topic != topic_stop_name_) {
-    topic_stop_name_ = new_topic;
-    publisher_stop_.reset();
-    publisher_stop_ = node_->create_publisher<std_msgs::msg::Empty>(
-      topic_stop_name_.toStdString(),
-      dua_qos::Reliable::get_datum_qos());
+  if (!new_topic_.isEmpty() && new_topic_ != stop_topic_) {
+    stop_topic_ = new_topic_;
+    init_stop_pub(stop_topic_.toStdString());
   }
 }
 
