37abba6e1bd36a6722a5e7299b894030457ac6fd
ErLex20 <alexandru.cretu@students.uniroma2.eu>
2025-02-25 14:30:39 +0100

    chore: added comments

--- include/dua_rviz_plugins/text_pub_panel.hpp
+++ include/dua_rviz_plugins/text_pub_panel.hpp
@@ -1,3 +1,28 @@
+/**
+ * Reference of RViz2 TextPubPanel for DUA modules.
+ *
+ * Alessandro Tenaglia <a.tenaglia@dotxautomation.com>
+ * Alexandru Cretu <a.cretu@dotxautomation.com>
+ *
+ *  February 17, 2025
+ */
+
+/**
+ * Copyright 2024 dotX Automation s.r.l.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef DUA_RVIZ_PLUGINS__TEXT_PUB_PANEL_HPP_
 #define DUA_RVIZ_PLUGINS__TEXT_PUB_PANEL_HPP_
 
--- src/text_pub_panel.cpp
+++ src/text_pub_panel.cpp
@@ -1,3 +1,28 @@
+/**
+ * Implementation of RViz2 TextPubPanel for DUA modules.
+ *
+ * Alessandro Tenaglia <a.tenaglia@dotxautomation.com>
+ * Alexandru Cretu <a.cretu@dotxautomation.com>
+ *
+ *  February 17, 2025
+ */
+
+/**
+ * Copyright 2024 dotX Automation s.r.l.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <dua_rviz_plugins/text_pub_panel.hpp>
 
 namespace dua_rviz_plugins
